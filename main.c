c9219fa2a93d5927822f3e44e46af7751a4717c0
Kainos-SU <62204744+Kainos-SU@users.noreply.github.com>
2022-10-26 21:35:41 +0300

    Alternative configuration to timers

--- main.c
+++ main.c
@@ -16,6 +16,51 @@ struct simpleButtonStruct {
     unsigned int debounceTimer: 14;
 };
 
+void timerInit() {
+    // Timer/Counter 0 initialization
+    // Clock source: System Clock
+    // Clock value: 187,500 kHz
+    // Mode: CTC top=OCR0A
+    // OC0A output: Disconnected
+    // OC0B output: Disconnected
+    // Timer Period: 0,8 ms
+    TCCR0A=(0<<COM0A1) | (0<<COM0A0) | (0<<COM0B1) | (0<<COM0B0) | (1<<WGM01) | (0<<WGM00);
+    TCCR0B=(0<<WGM02) | (0<<CS02) | (1<<CS01) | (1<<CS00);
+    TCNT0=0x00;
+    OCR0A=0x95;
+    OCR0B=0x00;
+
+    // Timer/Counter 1 initialization
+    // Clock source: System Clock
+    // Clock value: 12000,000 kHz
+    // Mode: CTC top=OCR1A
+    // OC1A output: Disconnected
+    // OC1B output: Disconnected
+    // Noise Canceler: Off
+    // Input Capture on Falling Edge
+    // Timer Period: 1 ms
+    // Timer1 Overflow Interrupt: Off
+    // Input Capture Interrupt: Off
+    // Compare A Match Interrupt: On
+    // Compare B Match Interrupt: Off
+    TCCR1A=(0<<COM1A1) | (0<<COM1A0) | (0<<COM1B1) | (0<<COM1B0) | (0<<WGM11) | (0<<WGM10);
+    TCCR1B=(0<<ICNC1) | (0<<ICES1) | (0<<WGM13) | (1<<WGM12) | (0<<CS12) | (0<<CS11) | (1<<CS10);
+    TCNT1H=0x00;
+    TCNT1L=0x00;
+    ICR1H=0x00;
+    ICR1L=0x00;
+    OCR1AH=0x2E;
+    OCR1AL=0xDF;
+    OCR1BH=0x00;
+    OCR1BL=0x00;
+
+    // Timer(s)/Counter(s) Interrupt(s) initialization
+    TIMSK=(0<<TOIE1) | (1<<OCIE1A) | (0<<OCIE1B) | (0<<ICIE1) | (0<<OCIE0B) | (0<<TOIE0) | (0<<OCIE0A);
+    
+    // Global enable interrupts
+    sei();
+}
+
 struct simpleButtonStruct incrementButton = {false, false, 0};
 struct simpleButtonStruct decrementButton = {false, false, 0};
 
