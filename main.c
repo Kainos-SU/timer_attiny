8a321c98526f52094f48c3a26c69f5aebe91b810
Kainos-SU <kapinos4@gmail.com>
2022-10-26 23:39:19 +0300

    Set buttons structures as volatile

--- main.c
+++ main.c
@@ -25,10 +25,8 @@ void timerInit() {
     // OC0B output: Disconnected
     // Timer Period: 0,8 ms
     TCCR0A=(0<<COM0A1) | (0<<COM0A0) | (0<<COM0B1) | (0<<COM0B0) | (1<<WGM01) | (0<<WGM00);
-    TCCR0B=(0<<WGM02) | (0<<CS02) | (1<<CS01) | (1<<CS00);
-    TCNT0=0x00;
-    OCR0A=0x95;
-    OCR0B=0x00;
+    TCCR0B=(0<<WGM02) | (0<<CS02) | (0<<CS01) | (1<<CS00);
+    OCR0A=150;
 
     // Timer/Counter 1 initialization
     // Clock source: System Clock
@@ -45,37 +43,28 @@ void timerInit() {
     // Compare B Match Interrupt: Off
     TCCR1A=(0<<COM1A1) | (0<<COM1A0) | (0<<COM1B1) | (0<<COM1B0) | (0<<WGM11) | (0<<WGM10);
     TCCR1B=(0<<ICNC1) | (0<<ICES1) | (0<<WGM13) | (1<<WGM12) | (0<<CS12) | (0<<CS11) | (1<<CS10);
-    TCNT1H=0x00;
-    TCNT1L=0x00;
-    ICR1H=0x00;
-    ICR1L=0x00;
     OCR1AH=0x2E;
     OCR1AL=0xDF;
-    OCR1BH=0x00;
-    OCR1BL=0x00;
 
     // Timer(s)/Counter(s) Interrupt(s) initialization
-    TIMSK=(0<<TOIE1) | (1<<OCIE1A) | (0<<OCIE1B) | (0<<ICIE1) | (0<<OCIE0B) | (0<<TOIE0) | (0<<OCIE0A);
-    
+    TIMSK=(0<<TOIE1) | (1<<OCIE1A) | (0<<OCIE1B) | (0<<ICIE1) | (0<<OCIE0B) | (0<<TOIE0) | (1<<OCIE0A);
+
     // Global enable interrupts
     sei();
 }
 
-struct simpleButtonStruct incrementButton = {false, false, 0};
-struct simpleButtonStruct decrementButton = {false, false, 0};
+volatile struct simpleButtonStruct incrementButton = {false, false, 0};
+volatile struct simpleButtonStruct decrementButton = {false, false, 0};
 
-bool handleSimpleButton(bool currentState, struct simpleButtonStruct* button) {
+bool handleSimpleButton(bool currentState, volatile struct simpleButtonStruct* button) {
+    bool result = false;
     if ((button->defaultState == button->prevState) && (button->prevState != currentState) && !(button->debounceTimer)) {
         button->debounceTimer = DEBOUNCE;
-        button->prevState = currentState;
-        return true;
+
+        result = true;
     }
-    if ((button->defaultState != button->prevState) && (button->prevState != currentState) && !(button->debounceTimer)) {
-        button->debounceTimer = DEBOUNCE;
-        button->prevState = currentState;
-        return false;
-    }
-    return false;
+    button->prevState = currentState;
+    return result;
 }
 
 ISR(TIMER0_COMPA_vect) {
@@ -88,7 +77,8 @@ ISR(TIMER1_COMPA_vect) {
     }
     if (incrementButton.debounceTimer) {
         --incrementButton.debounceTimer;
-    }else if (decrementButton.debounceTimer) {
+    }
+    if (decrementButton.debounceTimer) {
         --decrementButton.debounceTimer;
     }
 }
@@ -98,16 +88,10 @@ int main(void)
     DDRD = (1 << PIN5) | (1 << PIN6);
     PORTD = 0;
     initDispay();
-    OCR0A = 150;
-    TCCR0A |= 1 << WGM01;
-    TCCR0B |= 1 << CS00;
     incrementButton.defaultState = (PIND >> PIN4) & 1;
     decrementButton.defaultState = (PIND >> PIN3) & 1;
-    TCCR1B |= (1 << WGM12) | (1 << CS10) | (1 << CS11);
-    OCR1AL = 186;
-    OCR1AH = 0;
-    TIMSK = (1 << OCIE0A) | (1 << OCIE1A);
-    sei();
+    timerInit();
+    PORTB = 1;
     _delay_ms(200);
     blankDisplay(TIMER);
 
@@ -117,7 +101,9 @@ int main(void)
         }
         setCounterValue(counterToDisplay);
         if (handleSimpleButton((PIND >> PIN4) & 1, &incrementButton)) {
-            if (timerStarted) ++counterToDisplay;
+            if (timerStarted) {
+                ++counterToDisplay;
+            }
             timerStarted = true;
             milliseconds = 0;
         }
